e150c7b62b104f3effb9999a1f4e6550246a78b1
alvinahmd <alvintop11@gmail.com>
2023-06-26 11:00:27 +0700

    lagi dan lagi

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed9e0e0e479c4c13fa96aaecdfed6403467734d94b1f3b2f934e29a800fb39c2
-size 24672
+oid sha256:feca1701ddbf1a183bd6b851132ffdd804a00e8dd0c69c1f9738a56efa19b76c
+size 24276
