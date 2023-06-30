2161bcdf10cf355204f69983a2f60d6c955befc7
alvinahmd <alvintop11@gmail.com>
2023-06-30 17:45:27 +0700

    okee

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2455c86a05f4d4f1a8aeff2d361bb3284deae00c3e49d12331933cead03b590e
-size 24286
+oid sha256:a8e047c280bc853bbeda6b4d0d6c3aa699acf4ecee85672436e118798f4ecbae
+size 24589
