3f9611dc4860128507d67216f591282d0582ad35
alvinahmd <alvintop11@gmail.com>
2023-09-17 14:32:22 +0700

    smooth scroll

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffccfff8a004aefa612549152332ca07ebfba31c0c68e3bf744ec4e857fef6e5
-size 24531
+oid sha256:5f458dfe1f91f96ad2431cbf4b50c3ea884615304039ae07d488d6f25324d78c
+size 29031
